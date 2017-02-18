f775b7dd359779452bb8b5cd4e8e4b79058caad4
Rachel Ward <rward1137@gmail.com>
2017-02-18 12:01:33 -0500

    Create Movie.java

---
+++ Movie.java
@@ -0,0 +1,37 @@
+public class Movie {
+	
+	private String movieName;
+	private String movieRating;
+	private int movieID;
+	
+	public Movie() {}
+	
+	public Movie(String movieName, String movieRating, int movieID) {
+		this.movieName = movieName;
+		this.movieRating = movieRating;
+		this.movieID = (movieID);
+	}
+
+	public String getMovieName() {
+		return movieName;
+	}
+	public void setMovieName(String movieName) {
+		this.movieName = movieName;
+	}
+	
+	public String getMovieRating() {
+		return movieRating;
+	}
+	public void setMovieRating(String movieRating) {
+		this.movieRating = movieRating;
+	}
+
+	public int getMovieID() {
+		return movieID;
+	}
+
+	public void setMovieID(int movieID) {
+		this.movieID = movieID;
+	}
+
+}
